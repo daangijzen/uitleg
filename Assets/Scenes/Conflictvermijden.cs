5f6b30ba4bd0a6e33e24864a97679faf111c185b
daangijzen <daangijzen@gmail.com>
2025-11-11 15:04:13 +0100

    aanpassing op main

--- Assets/Scenes/Conflictvermijden.cs
+++ Assets/Scenes/Conflictvermijden.cs
@@ -5,7 +5,7 @@ public class Conflictvermijden : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Instantiate(gameObject);
     }
 
     // Update is called once per frame
